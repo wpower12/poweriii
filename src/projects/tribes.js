7104e092ff5ab841dc11fd15ed7c87ed181118f9
William Power <willpowe@gmail.com>
2017-08-08 14:52:30 -0400

    tribes working.  input working.

--- src/projects/tribes.js
+++ src/projects/tribes.js
@@ -236,7 +236,52 @@ class InputHandler {
     constructor (sim){
         this.sim = sim;
         // TODO - Replace all the onchange listeners with a giant key handler.
-        // same as with the traffic and forces things.       
+        // same as with the traffic and forces things.  
+        this.sim.canvas.on('keypress', this.handleKey.bind(this));      
+    }
+    handleKey (key){
+        switch(key.which){
+            case 113: // q fps down
+                if(this.sim.fps > 1){
+                    this.sim.fps--;
+                    $(this.rangefps).text(""+this.sim.fps);
+                }
+                break;
+            case 119:  // w - fps 'up'
+                if(this.sim.fps < 60){
+                    this.sim.fps++;
+                    $(this.rangefps).text(""+this.sim.fps);
+                }
+                break;
+            case 97:   // a - fill down
+                if(this.sim.fillfactor > 1){
+                    this.sim.fillfactor--;
+                    $(this.rangefill).text(""+this.sim.fillfactor);
+                    this.sim.reset();
+                }
+                break; 
+            case 115:  // s - fill up
+                if(this.sim.fillfactor < 100){
+                    this.sim.fillfactor++;
+                    $(this.rangefill).text(""+this.sim.fillfactor);
+                    this.sim.reset();    
+                }
+                break;
+            case 122:  // z - scale down
+                if(this.sim.scale > 5){
+                    this.sim.scale--;
+                    $(this.rangescale).text(""+this.sim.scale);
+                    this.sim.reset();
+                }
+                break; 
+            case 120:  // x - scale up
+                if(this.sim.scale < 20){
+                    this.sim.scale++;
+                    $(this.rangescale).text(""+this.sim.scale);
+                    this.sim.reset();
+                }
+                break;  
+        }
     }
     setFieldsFPS (r, v) {
         this.rangefps = r;
