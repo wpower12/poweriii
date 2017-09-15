d8305e0aec31919b291955df8fb312e6d6a74236
William Power <willpowe@gmail.com>
2017-09-15 11:34:17 -0400

    cleaning up any stuff

--- src/projects/antbrain/basic/basic_operate.js
+++ src/projects/antbrain/basic/basic_operate.js
@@ -11,9 +11,19 @@ class BasicOperate {
 		this.gen = new BasicGenerator();
 	}
 
+	// For now just returning the one. 
 	crossover(p1, p2){
+		// TODO - Need to play with copies of the parents, want to avoid issues with reselection.
 		// TODO - Implement with crossover
-		return [p1, p2];
+		var cp1 = get_crossover(p1);
+		var cp2 = get_crossover(p2);
+
+		if(cp1[2]){	// If p1's cp is on a truebranch
+			cp1[1].t_branch = cp2[0]; // Place the second trees cp node there.
+		} else {
+			cp1[1].f_branch = cp2[0];
+		}
+		return p1;
 	}
 
 	mutate(p){
@@ -21,5 +31,38 @@ class BasicOperate {
 		return p;
 	}
 }
+
+function get_crossover(p){
+	// Need max depth? Need to deal with non-full trees, too.
+	var cur_depth = 1;
+	var node   = p;	
+	var n = p;
+	var parent = null;
+	var branch = null;
+	var TEST_CHANCE = 0.5
+
+	while( n !== null ){
+		if(Math.random() < TEST_CHANCE){	// TODO - Make this not shitty.
+			parent = node;
+			if(Math.random() < 0.5){
+				branch = true;
+				node = node.t_branch;
+			} else {
+				branch = false;
+				node = node.f_branch;
+			}
+			n = null;
+		} else {
+			parent = n;
+			if(Math.random() < 0.5){
+				n = n.t_branch;
+			} else {
+				n = n.f_branch;
+			}
+		}
+	}
+	return [node, parent, branch];
+}
+
 
 export default BasicOperate;
--- src/projects/antbrain/gp_experiment.js
+++ src/projects/antbrain/gp_experiment.js
@@ -15,10 +15,10 @@ class Experiment {
 		this.generations = 50;
 		this.runs_per_eval = 5;
 	}
+
 	run(){
 		this.population = build_population(this.gen, this.size, this.max_depth);
 		for( var g = 0; g < this.generations; g++){
-			console.log(this.population.length);
 			this.fitness    = evaluate(this.sim, this.population, this.runs_per_eval);
 			this.population = select_operate(this.op, this.fitness, this.population);
 		}
@@ -28,7 +28,6 @@ class Experiment {
 		// Last element in pop is best.
 		this.fitness    = evaluate(this.sim, this.population, this.runs_per_eval);
 		this.sim.simulate_canvas(this.population[this.size-1], canvas);
-		console.log(this.fitness);
 	}
 }
 
@@ -39,7 +38,7 @@ function build_population(gen, size, max_depth){
 	for( var depth = 1; depth <= num_buckets; depth++ ){
 		for( var n = 0; n < bucket_size; n++){
 			// Each loop we add a full and a partial tree. 
-			// Brings use to a total of 'size'
+			// Brings us to a total of 'size'
 			ret.push( gen.generate(depth, "full") );
 			ret.push( gen.generate(depth, "partial"));
 		}
@@ -61,28 +60,55 @@ function evaluate(sim, pop, rpe){
 }
 
 function select_operate(op, fit, pop){
-	// TODO - Actually implement.  Just returns the sorted population right now. 
-	var pop_zip_sort = zip(pop, fit).sort(fit_cmp); 
-	return unzip(pop_zip_sort);
+	var pop_zip = zip(pop, fit); 
+	var new_pop = [];
+	for(var i = 0; i < fit.length; i++){
+		var p1 = select_fps(pop_zip);
+		var p2 = select_fps(pop_zip);
+		new_pop = new_pop.concat( crossover(p1,p2) );
+	}
+	console.log(new_pop.length);
+	return new_pop;
 }
 
-function zip( pop, fit ){
+function select_fps(fit_pop){
+	var total = 0;
+	for( var i = 0; i < fit_pop.length; i++ ){
+		total += fit_pop[i].f;
+	}
+	var r = Math.random()*total;
+	for( var i = 0; i < fit_pop.length; i++	){
+		r -= fit_pop[i].f;
+		if(r <= 0) return fit_pop[i].ind;
+	}
+	return fit_pop.length - 1;
+}
+
+function crossover(p1, p2){
+	// TODO - Implement tree based genetic crossover.
+	// Traverse each, randomly select a node weighted by depth?
+	// How to traverse and have an equal chance to pick a node 
+	// from each depth?
+	return [p1, p2];
+}
+
+function zip( l1, l2 ){
 	var ret = [];
-	for( var i = 0; i < pop.length; i++ ){
-		var item = {ind: pop[i], f: fit[i]};
+	for( var i = 0; i < l1.length; i++ ){
+		var item = {ind: l1[i], f: l2[i]};
 		ret.push(item);
 	}
 	return ret;
 }
 
 function unzip(l){
-	var pop = [];
-	var fit = [];
+	var l1 = [];
+	var l2 = [];
 	for(var i = 0; i < l.length; i++){
-		pop.push(l[i].ind);
-		fit.push(l[i].f);
+		l1.push(l[i].ind);
+		l2.push(l[i].f);
 	}
-	return pop;
+	return [l1,l2];
 }
 
 function fit_cmp(a, b){
